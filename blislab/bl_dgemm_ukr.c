a2cfdddbd7fbb80321df1dcd045a1099fe11968c
Abhishek2304 <abhishekgupta2304@gmail.com>
2022-04-21 12:03:09 -0700

    Added loop unrolling to the microkernel

--- blislab/bl_dgemm_ukr.c
+++ blislab/bl_dgemm_ukr.c
@@ -104,41 +104,127 @@ void gemm_avx( int k,
     //     }
     // }
 
-    for (int e = 0; e < 2; e++){
+            
+    // 0,0
+    register __m256d c00_c01_c02_c03 = _mm256_loadu_pd(c + 4*0 + ldc*4*0);
+    register __m256d c10_c11_c12_c13 = _mm256_loadu_pd(c + ldc + 4*0 + ldc*4*0);
+    register __m256d c20_c21_c22_c23 = _mm256_loadu_pd(c + 2*ldc + 4*0 + ldc*4*0);
+    register __m256d c30_c31_c32_c33 = _mm256_loadu_pd(c + 3*ldc + 4*0 + ldc*4*0);
 
-        for (int f = 0; f < 2; f++){
+    for(int i = 0; i < k; i++){
 
-            register __m256d c00_c01_c02_c03 = _mm256_loadu_pd(c + 4*f + ldc*4*e);
-            register __m256d c10_c11_c12_c13 = _mm256_loadu_pd(c + ldc + 4*f + ldc*4*e);
-            register __m256d c20_c21_c22_c23 = _mm256_loadu_pd(c + 2*ldc + 4*f + ldc*4*e);
-            register __m256d c30_c31_c32_c33 = _mm256_loadu_pd(c + 3*ldc + 4*f + ldc*4*e);
+        register __m256d a0x = _mm256_broadcast_sd(&a[i*m + 4*0 + 0]);
+        register __m256d a1x = _mm256_broadcast_sd(&a[i*m + 4*0 + 1]);
+        register __m256d a2x = _mm256_broadcast_sd(&a[i*m + 4*0 + 2]);
+        register __m256d a3x = _mm256_broadcast_sd(&a[i*m + 4*0 + 3]);
+        
+        temp[0] = b[i*n + 4*0 + 0];
+        temp[1] = b[i*n + 4*0 + 1];
+        temp[2] = b[i*n + 4*0 + 2];
+        temp[3] = b[i*n + 4*0 + 3];
+        register __m256d matrix2 = _mm256_loadu_pd(&temp[0]);
 
-            for(int i = 0; i < k; i++){
-
-                register __m256d a0x = _mm256_broadcast_sd(&a[i*m + 4*e + 0]);
-                register __m256d a1x = _mm256_broadcast_sd(&a[i*m + 4*e + 1]);
-                register __m256d a2x = _mm256_broadcast_sd(&a[i*m + 4*e + 2]);
-                register __m256d a3x = _mm256_broadcast_sd(&a[i*m + 4*e + 3]);
-                
-                temp[0] = b[i*n + 4*f + 0];
-                temp[1] = b[i*n + 4*f + 1];
-                temp[2] = b[i*n + 4*f + 2];
-                temp[3] = b[i*n + 4*f + 3];
-                register __m256d matrix2 = _mm256_loadu_pd(&temp[0]);
-
-                c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, matrix2, c00_c01_c02_c03);
-                c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, matrix2, c10_c11_c12_c13);
-                c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, matrix2, c20_c21_c22_c23);
-                c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, matrix2, c30_c31_c32_c33);
-            }
-
-            _mm256_storeu_pd(c + 4*f + ldc*4*e, c00_c01_c02_c03);
-            _mm256_storeu_pd(c + ldc + 4*f + ldc*4*e, c10_c11_c12_c13);
-            _mm256_storeu_pd(c + 2*ldc + 4*f + ldc*4*e, c20_c21_c22_c23);
-            _mm256_storeu_pd(c + 3*ldc + 4*f + ldc*4*e, c30_c31_c32_c33);
-        }
+        c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, matrix2, c00_c01_c02_c03);
+        c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, matrix2, c10_c11_c12_c13);
+        c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, matrix2, c20_c21_c22_c23);
+        c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, matrix2, c30_c31_c32_c33);
     }
 
+    _mm256_storeu_pd(c + 4*0 + ldc*4*0, c00_c01_c02_c03);
+    _mm256_storeu_pd(c + ldc + 4*0 + ldc*4*0, c10_c11_c12_c13);
+    _mm256_storeu_pd(c + 2*ldc + 4*0+ ldc*4*0, c20_c21_c22_c23);
+    _mm256_storeu_pd(c + 3*ldc + 4*0 + ldc*4*0, c30_c31_c32_c33);
+
+    //0,1
+    c00_c01_c02_c03 = _mm256_loadu_pd(c + 4*1 + ldc*4*0);
+    c10_c11_c12_c13 = _mm256_loadu_pd(c + ldc + 4*1 + ldc*4*0);
+    c20_c21_c22_c23 = _mm256_loadu_pd(c + 2*ldc + 4*1 + ldc*4*0);
+    c30_c31_c32_c33 = _mm256_loadu_pd(c + 3*ldc + 4*1 + ldc*4*0);
+
+    for(int i = 0; i < k; i++){
+
+        register __m256d a0x = _mm256_broadcast_sd(&a[i*m + 4*0 + 0]);
+        register __m256d a1x = _mm256_broadcast_sd(&a[i*m + 4*0+ 1]);
+        register __m256d a2x = _mm256_broadcast_sd(&a[i*m + 4*0 + 2]);
+        register __m256d a3x = _mm256_broadcast_sd(&a[i*m + 4*0+ 3]);
+        
+        temp[0] = b[i*n + 4*1 + 0];
+        temp[1] = b[i*n + 4*1 + 1];
+        temp[2] = b[i*n + 4*1 + 2];
+        temp[3] = b[i*n + 4*1 + 3];
+        register __m256d matrix2 = _mm256_loadu_pd(&temp[0]);
+
+        c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, matrix2, c00_c01_c02_c03);
+        c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, matrix2, c10_c11_c12_c13);
+        c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, matrix2, c20_c21_c22_c23);
+        c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, matrix2, c30_c31_c32_c33);
+    }
+
+    _mm256_storeu_pd(c + 4*1 + ldc*4*0, c00_c01_c02_c03);
+    _mm256_storeu_pd(c + ldc + 4*1 + ldc*4*0, c10_c11_c12_c13);
+    _mm256_storeu_pd(c + 2*ldc + 4*1 + ldc*4*0, c20_c21_c22_c23);
+    _mm256_storeu_pd(c + 3*ldc + 4*1 + ldc*4*0, c30_c31_c32_c33);
+
+    //1,0
+    c00_c01_c02_c03 = _mm256_loadu_pd(c + 4*0 + ldc*4*1);
+    c10_c11_c12_c13 = _mm256_loadu_pd(c + ldc + 4*0 + ldc*4*1);
+    c20_c21_c22_c23 = _mm256_loadu_pd(c + 2*ldc + 4*0 + ldc*4*1);
+    c30_c31_c32_c33 = _mm256_loadu_pd(c + 3*ldc + 4*0 + ldc*4*1);
+
+    for(int i = 0; i < k; i++){
+
+        register __m256d a0x = _mm256_broadcast_sd(&a[i*m + 4*1 + 0]);
+        register __m256d a1x = _mm256_broadcast_sd(&a[i*m + 4*1 + 1]);
+        register __m256d a2x = _mm256_broadcast_sd(&a[i*m + 4*1 + 2]);
+        register __m256d a3x = _mm256_broadcast_sd(&a[i*m + 4*1 + 3]);
+        
+        temp[0] = b[i*n + 4*0 + 0];
+        temp[1] = b[i*n + 4*0 + 1];
+        temp[2] = b[i*n + 4*0 + 2];
+        temp[3] = b[i*n + 4*0+ 3];
+        register __m256d matrix2 = _mm256_loadu_pd(&temp[0]);
+
+        c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, matrix2, c00_c01_c02_c03);
+        c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, matrix2, c10_c11_c12_c13);
+        c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, matrix2, c20_c21_c22_c23);
+        c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, matrix2, c30_c31_c32_c33);
+    }
+
+    _mm256_storeu_pd(c + 4*0 + ldc*4*1, c00_c01_c02_c03);
+    _mm256_storeu_pd(c + ldc + 4*0 + ldc*4*1, c10_c11_c12_c13);
+    _mm256_storeu_pd(c + 2*ldc + 4*0 + ldc*4*1, c20_c21_c22_c23);
+    _mm256_storeu_pd(c + 3*ldc + 4*0 + ldc*4*1, c30_c31_c32_c33);
+
+    //1,1
+
+    c00_c01_c02_c03 = _mm256_loadu_pd(c + 4*1 + ldc*4*1);
+    c10_c11_c12_c13 = _mm256_loadu_pd(c + ldc + 4*1 + ldc*4*1);
+    c20_c21_c22_c23 = _mm256_loadu_pd(c + 2*ldc + 4*1 + ldc*4*1);
+    c30_c31_c32_c33 = _mm256_loadu_pd(c + 3*ldc + 4*1 + ldc*4*1);
+
+    for(int i = 0; i < k; i++){
+
+        register __m256d a0x = _mm256_broadcast_sd(&a[i*m + 4*1 + 0]);
+        register __m256d a1x = _mm256_broadcast_sd(&a[i*m + 4*1 + 1]);
+        register __m256d a2x = _mm256_broadcast_sd(&a[i*m + 4*1 + 2]);
+        register __m256d a3x = _mm256_broadcast_sd(&a[i*m + 4*1 + 3]);
+        
+        temp[0] = b[i*n + 4*1 + 0];
+        temp[1] = b[i*n + 4*1 + 1];
+        temp[2] = b[i*n + 4*1 + 2];
+        temp[3] = b[i*n + 4*1 + 3];
+        register __m256d matrix2 = _mm256_loadu_pd(&temp[0]);
+
+        c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, matrix2, c00_c01_c02_c03);
+        c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, matrix2, c10_c11_c12_c13);
+        c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, matrix2, c20_c21_c22_c23);
+        c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, matrix2, c30_c31_c32_c33);
+    }
+
+    _mm256_storeu_pd(c + 4*1 + ldc*4*1, c00_c01_c02_c03);
+    _mm256_storeu_pd(c + ldc + 4*1 + ldc*4*1, c10_c11_c12_c13);
+    _mm256_storeu_pd(c + 2*ldc + 4*1 + ldc*4*1, c20_c21_c22_c23);
+    _mm256_storeu_pd(c + 3*ldc + 4*1 + ldc*4*1, c30_c31_c32_c33);
 
                 
                 // ymm[4] = _mm256_mul_pd(ymm[0], matrix2);
